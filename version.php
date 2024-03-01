8b7ed2395b9bba93ec585fc8b28f0c8a247c76d7
Céline Pervès <cperves@unistra.fr>
2024-03-01 11:03:06 +0100

    #7 new version, release

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_reacsimilarity';
-$plugin->version   = 2024021500;
+$plugin->version   = 2024030100;
 $plugin->requires  = 2020060900;
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.3';
+$plugin->release = '1.3.1';
